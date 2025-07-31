6462a6b314e8e6efa46fc7a08eb926bc95381ae0
Vikram Kannan <110043183+shivalingeshwar6@users.noreply.github.com>
2025-07-31 14:28:12 +0530

    Delete config directory

--- config/bin/eth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-IFACE=$(/usr/sbin/ifconfig | grep eth0 | awk '{print $1}' | tr -d ':')
-
-if [ "$IFACE" = "eth0" ]; then
-        echo "%{F#1bbf3e} %{F#ffffff}$(/usr/sbin/ifconfig eth0 | grep "inet " | awk '{print $2}')%{u-}"
-else
-        echo "%{F#2495e7}%{u-} %{F#ffffff}Disconnected"
-fi
